95aa44a0c103d064a8fcc9b9b52ca772bae1792a
Kamil Mówiński <kamil.mowinski@vulpesoft.pl>
2020-06-04 13:51:16 +0200

    Mailchimp

--- header.php
+++ header.php
@@ -8,6 +8,7 @@
     <?php
     wp_head();
     ?>
+    <script id="mcjs">!function(c,h,i,m,p){m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)}(document,"script","https://chimpstatic.com/mcjs-connected/js/users/69200756dcd519f5be6846809/f3cff81f6e29c1ada36734506.js");</script>
 </head>
 
 <body>
